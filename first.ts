829254ae3e7d933aad3f993a3fcf3720e3366fd9
Kerim-tk <alekperovkerim@gmail.com>
2022-11-13 20:24:45 +0400

    Revert "Initial commit"
    This reverts commit 2852b6af4cccf461182fc7f349b131ccabf373ae.

--- first.ts
+++ first.ts
@@ -1,3 +1,11 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
+function request(){
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(function(response){
+    return response.json()
+  })
+  .then((response) => {
+    console.log(response)
+    let result = document.getElementById('result')
+    result?.innerHTML 
+  })
+}
